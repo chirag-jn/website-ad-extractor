98941b9712d738e4dd9a510622a8b095d0358827
Chirag Jain <jainchg@gmail.com>
2021-02-08 01:31:06 +0530

    Web extraction complete

---
+++ constants.py
@@ -0,0 +1,9 @@
+operators = ['airtel', 'vi', 'jio', 'tatasky']
+
+web_urls = {
+    operators[0] : 'https://www.airtel.in/',
+    operators[1] : 'https://www.myvi.in/',
+    operators[2] : 'https://www.jio.com/',
+    operators[3] : 'https://www.tatasky.com/dth/'
+}
+
--- main.py
+++ main.py
@@ -1,13 +1,84 @@
 import time
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
+from constants import operators, web_urls
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urljoin
 
-opt = Options()
-# opt.set_headless()
+def getAirtel():
 
-# assert opt.headless
+    global web_urls, operators
 
-browser = Chrome('drivers/chromedriver_win32/chromedriver.exe', options=opt)
-browser.get('https://google.com')
-time.sleep(5)
-browser.quit()
+    response = requests.get(web_urls[operators[0]]).text
+
+    soup = BeautifulSoup(response, features='html.parser')
+
+    img_container = soup.findAll('div', {'class': 'slide-container'})
+
+    img_urls = []
+    for i in img_container:
+        img = i.find('img')
+        img_urls.append(img['data-banner-src'])
+    
+    return img_urls
+
+def getJio():
+
+    global web_urls, operators
+
+    response = requests.get(web_urls[operators[2]]).text
+
+    soup = BeautifulSoup(response, features='html.parser')
+    div = soup.find('div', {'class': 'home_banner_slider'})
+
+    a_container = div.findAll('a')
+
+    img_urls = []
+    for i in a_container:
+        img = i.find('img', {'class': 'load_img_for_desktop'})
+        img_urls.append(urljoin(web_urls[operators[2]], img['data-src-load']))
+    
+    return img_urls
+
+def getVi():
+
+    global web_urls, operators
+
+    response = requests.get(web_urls[operators[1]])
+
+    response.encoding = response.apparent_encoding
+
+    response = str(response.text)
+
+    soup = BeautifulSoup(response, features='html.parser')
+    div = soup.find('div', {'class': 'vi_slider'})
+
+    source_container = div.findAll('source',  {'media': '(min-width:768px)'})
+
+    img_urls = []
+    for i in source_container:
+        img_urls.append(urljoin(web_urls[operators[1]], i['data-lazy-srcset']))
+    
+    return img_urls
+
+def getTataSky():
+
+    global web_urls, operators
+
+    response = requests.get(web_urls[operators[3]])
+
+
+
+    response.encoding = response.apparent_encoding
+
+    response = str(response.text)
+
+    soup = BeautifulSoup(response, features='html.parser')
+    div = soup.find('div', {'class': 'offer-card-outer'})
+
+    img_container = div.findAll('img')
+
+    img_urls = []
+    for i in img_container:
+        img_urls.append(urljoin(web_urls[operators[3]], i['src']))
+    
+    return img_urls
