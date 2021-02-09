e63c42b0ce3d2bb5c7d241ce8a1e10250e2ac342
Chirag Jain <jainchg@gmail.com>
2021-02-09 22:16:13 +0530

    Image upload done

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
-import time
+import os
+from datetime import timezone, datetime
 from constants import operators, web_urls
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urljoin
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
+
+bucket = None
+
+def getTime():
+    return str(int(datetime.now(tz=timezone.utc).timestamp() * 1000))
+
+def initFirebase():
+    global bucket
+    cred = credentials.Certificate('serviceAccountKey.json')
+    firebase_admin.initialize_app(cred, {
+    'storageBucket': 'fuse-9747a.appspot.com'})
+    bucket = storage.bucket()
+
+def fileUpload(case, path):
+    global bucket
+    if not os.path.exists('images'):
+        os.makedirs('images')
+
+    r = requests.get(path, allow_redirects=True)
+    fileTime = getTime()
+    fileName = operators[case] + '-' + fileTime + '.' + path.split('.')[-1]
+    fullFileName = os.path.join('images', fileName)
+    open(fullFileName, 'wb+').write(r.content)
+
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fullFileName)
+
+    db = firestore.client()
+
+    doc_ref = db.collection('images').document()
+    doc_ref.create({
+        'operator': operators[case], 
+        'time': datetime.utcfromtimestamp(int(fileTime)//1000).strftime("%Y-%m-%d %H:%M:%S"),
+        'url': blob.public_url
+    })
 
 def getAirtel():
 
@@ -17,7 +55,8 @@ def getAirtel():
     img_urls = []
     for i in img_container:
         img = i.find('img')
-        img_urls.append(img['data-banner-src'])
+        url = img['data-banner-src'].split('?')[0]
+        img_urls.append(url)
     
     return img_urls
 
@@ -66,8 +105,6 @@ def getTataSky():
 
     response = requests.get(web_urls[operators[3]])
 
-
-
     response.encoding = response.apparent_encoding
 
     response = str(response.text)
@@ -82,3 +119,22 @@ def getTataSky():
         img_urls.append(urljoin(web_urls[operators[3]], i['src']))
     
     return img_urls
+
+def getUrls(case):
+    if case == 0:
+        return getAirtel()
+    elif case == 1:
+        return getVi()
+    elif case == 2:
+        return getJio()
+    elif case == 3:
+        return getTataSky()
+
+def uploadImage():
+    for case in range(len(operators)):
+        urls = getUrls(case)
+        for i in urls:
+            fileUpload(case, i)
+
+initFirebase()
+uploadImage()
