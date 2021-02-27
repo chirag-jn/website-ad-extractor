2ccefd26d6fcd7ba0b47703e183cdb5ab81669da
Chirag Jain <jainchg@gmail.com>
2021-02-28 01:45:39 +0530

    refactor code for ss

--- mobile/main.py
+++ mobile/main.py
@@ -3,8 +3,14 @@ import time
 import platform
 
 app_name = {
-    'airtel': 'com.myairtelapp'
+    'airtel': 'com.myairtelapp',
+    'jio': 'com.jio.myjio',
+    'tatasky': 'com.ryzmedia.tatasky',
+    'vi': 'com.mventus.selfcare.activity'
 }
+
+def getTime():
+    return str(int(datetime.now(tz=timezone.utc).timestamp() * 1000))
 
 def getPath():
     is_windows = any(platform.win32_ver())
@@ -20,21 +26,42 @@ def getPath():
 
     return path
 
+def connect_device(is_usb):
+    if not is_usb:
+        # Change Port
+        os.popen('adb tcpip 5555')
+
+        device = os.popen("adb devices").read().split('\n', 1)[1].split("device")[0].strip()
+
+        print('Waiting for connection ...')
+        connect = os.popen('adb connect ' + device).read()
+        print(connect)
+
+def extractAirtel():
+    global app_name
+
+    operator = 'airtel'
+
+    os.system('adb shell monkey -p ' + app_name[operator] + ' -c android.intent.category.LAUNCHER 1')
+
+    # Wait while the app opens
+    time.sleep(3)
+
+    screenshotNum = 0
+    curTime = getTime()
+
+    os.system('adb shell input swipe 300 1200 300 800')
+    
+    os.system('adb shell input swipe 300 800 200 800')
+
+    fileName = operator + '-' + getTime() + '.png'
+
+    fullFileName = os.path.join('mobile', 'images', fileName)
+
+    # adb exec-out screencap -p > screen.png
+
+
 os.chdir(getPath())
 
-is_local = True
-
-if not is_local:
-    # Change Port
-    os.popen('adb tcpip 5555')
-
-    device = os.popen("adb devices").read().split('\n', 1)[1].split("device")[0].strip()
-
-    print('Waiting for connection ...')
-    connect = os.popen('adb connect ' + device ).read()
-    print(connect)
-
-os.system('adb shell monkey -p ' + app_name['airtel'] + ' -c android.intent.category.LAUNCHER 1')
-
-# Wait while the app opens
-time.sleep(3)
+connect_device(is_usb=True)
+extractAirtel()
