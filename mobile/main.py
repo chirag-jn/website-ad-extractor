5eaf35cab1b39e6bbaff9f53f9b5ab4b0e2b0426
Chirag Jain <jainchg@gmail.com>
2021-03-29 23:39:23 +0530

    add multi device support

--- mobile/main.py
+++ mobile/main.py
@@ -2,6 +2,8 @@ import os
 import time
 import platform
 from datetime import datetime, timezone
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 app_name = {
     'airtel': 'com.myairtelapp',
@@ -11,6 +13,26 @@ app_name = {
 }
 
 cur_dir = os.getcwd()
+
+db = None
+phones = []
+ips = []
+
+def initFirebase():
+    global db
+    cred = credentials.Certificate('../serviceAccountKey.json')
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    print('Database Connected')
+
+def getIPAddr():
+    global db, phones, ips
+    docs = db.collection('ip-debug').stream()
+    phones = []
+    ips = []
+    for doc in docs:
+        phones.append(doc.id)
+        ips.append(doc.to_dict()['ip'])
 
 def getTime():
     return str(int(datetime.now(tz=timezone.utc).timestamp() * 1000))
@@ -30,7 +52,10 @@ def getPath():
     return path
 
 def connect_device(is_usb):
-    if not is_usb:
+
+    global ips
+
+    if is_usb:
         # Change Port
         os.popen('adb tcpip 5555')
 
@@ -39,6 +64,16 @@ def connect_device(is_usb):
         print('Waiting for connection ...')
         connect = os.popen('adb connect ' + device).read()
         print(connect)
+    
+    else:
+        # TODO: Error handling
+        for ip in range(len(ips)):
+            print('Waiting for connection ...')
+            connect = os.popen('adb connect ' + ips[ip]).read()
+            print(connect)
+
+def disconnect_device():
+    os.popen('adb disconnect')
 
 def extractAirtel():
     global app_name, cur_dir
@@ -120,8 +155,12 @@ def extractJio():
 
         os.system('adb shell input swipe 600 600 ' + str(x2) + ' 600')
 
+initFirebase()
+getIPAddr()
+
 os.chdir(getPath())
 
-connect_device(is_usb=True)
-extractAirtel()
-extractJio()
+connect_device(is_usb=False)
+# extractAirtel()
+# extractJio()
+disconnect_device()
