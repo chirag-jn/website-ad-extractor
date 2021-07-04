ca3aea061179d7eea9e2503b41cb1ad9736c443a
Chirag Jain <jainchg@gmail.com>
2021-07-04 21:57:01 +0530

    Email script complete with regex

--- email/main.py
+++ email/main.py
@@ -3,14 +3,18 @@ from firebase_admin import credentials, firestore
 import imaplib
 import email
 from email.header import decode_header
-import webbrowser
 import os
 import sys
 import pandas as pd
 from tqdm.auto import tqdm
+import html2text
+import re
 
 db = None
 _imap = None
+htmlHandler = html2text.HTML2Text()
+htmlHandler.ignore_links = True
+htmlHandler.escape_snob = True
 
 def initFirebase():
     global db
@@ -18,10 +22,6 @@ def initFirebase():
     firebase_admin.initialize_app(cred)
     db = firestore.client()
     print('Database Connected')
-
-def getEmailIds():
-    df = pd.read_csv('emails.csv')
-    return df
 
 def getImap():
     global _imap
@@ -42,7 +42,8 @@ def getDbName():
 
 def writeEmailsToFirebase():
     global db
-    for i, row in tqdm(getEmailIds().iterrows()):
+    df = pd.read_csv('emails.csv')
+    for i, row in tqdm(df.iterrows()):
         email_id = row['email']
         password = row['password']
         mobile = row['mobile']
@@ -64,12 +65,32 @@ def getEmailIdsFromFirebase():
 def clean(text):
     return "".join(c if c.isalnum() else "_" for c in text)
 
+def checkIfMailContains(body):
+    operators = ['airtel', 'vodafone', 'voda', 'jio', 'tatasky', 'idea', 'hotstar', 'netflix', 
+                 'ullu', 'prime', 'video', 'zee5', 'voot', 'sony', 'sonyliv', 'eros', 'jiocinema', 'tvfplay']
+    operator_keys = ['airtel', 'vi', 'vi', 'jio', 'tata', 'vi', 'hotstar', 'netflix', 
+                     'ullu', 'primevideo', 'primevideo', 'zee5', 'voot', 'sony', 'sony', 'eros', 'jio', 'tvfplay']
+
+    leftRe = '(^|[^a-z]+)'
+    rightRe = '($|[^a-z]+)'
+
+    for i in range(len(operators)):
+        pattern = re.compile(leftRe + operators[i])
+        if bool(pattern.search(body)):
+            return operator_keys[i]
+        
+    pattern = re.compile(leftRe + 'vi' + rightRe)
+    if bool(pattern.search(body)):
+        return 'vi'
+    
+    return False
+
 def getEmails(cred):
+
+    global htmlHandler, db
 
     email_id = cred['email']
     password = cred['password']
-
-    # print('\nWorking on:', email_id)
     
     imap = getImap()
     
@@ -83,29 +104,44 @@ def getEmails(cred):
     status, messages = imap.select("INBOX")
     N = 1000
     num_messages = int(messages[0])
-    # print(num_messages)
     N = num_messages
 
-    for i in tqdm(range(num_messages,num_messages - N, -1), leave=False):
+    for i in tqdm(range(num_messages,num_messages - N, -1)):
 
         res, msg = imap.fetch(str(i), "(RFC822)")
         for response in msg:
             if isinstance(response, tuple):
-                # parse a bytes email into a message object
+
+                mail_object = {
+                    'id': '',
+                    'time': '',
+                    'subject': '',
+                    'receiver': '',
+                    'sender': '',
+                    'body': '',
+                    'reason': ''
+                }
+
                 msg = email.message_from_bytes(response[1])
-                # decode the email subject
                 subject, encoding = decode_header(msg["Subject"])[0]
+                date, encoding = decode_header(msg["Date"])[0]
+                receiver, encoding = decode_header(msg["To"])[0]
+
+                mail_object['subject'] = str(subject)
+                mail_object['time'] = str(date)
+                mail_object['receiver'] = str(receiver)
+
                 if isinstance(subject, bytes):
-                    # if it's a bytes, decode to str
-                    subject = subject.decode(encoding)
-                # decode email sender
+                    try:
+                        subject = subject.decode(encoding)
+                    except:
+                        subject = str(subject)
                 From, encoding = decode_header(msg.get("From"))[0]
                 if isinstance(From, bytes):
                     From = From.decode(encoding)
                 
-                print("Subject:", subject)
-                print("From:", From)
-                # if the email message is multipart
+                mail_object['sender'] = str(From)
+
                 if msg.is_multipart():
                     # iterate over email parts
                     for part in msg.walk():
@@ -113,15 +149,15 @@ def getEmails(cred):
                         content_type = part.get_content_type()
                         content_disposition = str(part.get("Content-Disposition"))
                         try:
-                            # get the email body
                             body = part.get_payload(decode=True).decode()
                         except:
                             pass
                         if content_type == "text/plain" and "attachment" not in content_disposition:
-                            # print text/plain emails and skip attachments
-                            print(body)
+                            mail_object['body'] += '\n' + str(htmlHandler.handle(body))
+
                         elif "attachment" in content_disposition:
-                            # download attachment
+                            continue
+                            # attachment not needed
                             filename = part.get_filename()
                             if filename:
                                 folder_name = clean(subject)
@@ -132,27 +168,22 @@ def getEmails(cred):
                                 # download attachment and save it
                                 open(filepath, "wb").write(part.get_payload(decode=True))
                 else:
-                    # extract content type of email
                     content_type = msg.get_content_type()
-                    # get the email body
                     body = msg.get_payload(decode=True).decode()
                     if content_type == "text/plain":
-                        # print only text email parts
-                        print(body)
+                        mail_object['body'] += '\n' + str(htmlHandler.handle(body))
+
                 if content_type == "text/html":
-                    # if it's HTML, create a new HTML file and open it in browser
-                    folder_name = clean(subject)
-                    if not os.path.isdir(folder_name):
-                        # make a folder for this email (named after the subject)
-                        os.mkdir(folder_name)
-                    filename = "index.html"
-                    filepath = os.path.join(folder_name, filename)
-                    # write the file
-                    open(filepath, "w").write(body)
-                    # open in the default browser
-                    webbrowser.open(filepath)
-                print("="*100)
-    # close the connection and logout
+                    mail_object['body'] += '\n' + str(htmlHandler.handle(body))
+
+                doesMailContain = checkIfMailContains(mail_object['body'].lower())
+                if doesMailContain:
+                    mail_object['reason'] = doesMailContain
+                    mail_id = str(list(mail_object['time'].split(','))[1]) + ' --- ' + str(list(mail_object['receiver'].split('@'))[0])
+                    mail_object['id'] = mail_id
+                    doc_ref = db.collection('emails').document(mail_id)
+                    doc_ref.set(mail_object)
+
     imap.close()
     imap.logout()
 
@@ -160,5 +191,6 @@ def getEmails(cred):
 if __name__ == '__main__':
     initFirebase()
     creds = getEmailIdsFromFirebase()
-    for cred in tqdm(creds):
+    for cred in creds:
+        print('Parsing Email:', cred['email'])
         getEmails(cred)
